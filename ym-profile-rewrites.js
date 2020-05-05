0fe3de3f6a975d4d54836312cad532e07493e327
Justin Prevatte <justin@theme-logic.com>
2020-05-05 10:28:19 -0400

    js to update fonteva profile links to ym profile links

---
+++ ym-profile-rewrites.js
@@ -0,0 +1,3 @@
+/* REWRITES FONTEVA PROFILE LINKS TO YM PROFILE LINKS */
+$('a[href*="a0df4000000EvkkAAC"]:contains(Update Contact Information)').attr('href','https://members.simnet.org/members/edit.asp');
+$('a[href*="renew-redirect"], a[href*="a0df4000000EvkkAAC"]').attr('href','https://members.simnet.org/members/manage_profile.asp');
