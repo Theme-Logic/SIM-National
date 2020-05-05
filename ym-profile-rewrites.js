da6e0bd1157e162319a197238a873f5b5052479e
Justin Prevatte <justin@theme-logic.com>
2020-05-05 10:32:15 -0400

    forgot to wrap in document.ready

--- ym-profile-rewrites.js
+++ ym-profile-rewrites.js
@@ -1,3 +1,5 @@
 /* REWRITES FONTEVA PROFILE LINKS TO YM PROFILE LINKS */
-$('a[href*="a0df4000000EvkkAAC"]:contains(Update Contact Information)').attr('href','https://members.simnet.org/members/edit.asp');
-$('a[href*="renew-redirect"], a[href*="a0df4000000EvkkAAC"]').attr('href','https://members.simnet.org/members/manage_profile.asp');
+$(document).ready(fuction(){
+	$('a[href*="a0df4000000EvkkAAC"]:contains(Update Contact Information)').attr('href','https://members.simnet.org/members/edit.asp');
+	$('a[href*="renew-redirect"], a[href*="a0df4000000EvkkAAC"]').attr('href','https://members.simnet.org/members/manage_profile.asp');
+});
