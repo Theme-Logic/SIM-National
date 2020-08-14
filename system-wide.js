824373e505037ccdff88c0f47e211ef0bfa6d6d9
Justin Prevatte <justin@theme-logic.com>
2020-08-14 10:52:54 -0400

    footer fix added to system-wide.js

--- system-wide.js
+++ system-wide.js
@@ -1,6 +1,7 @@
-/* EDIT EVENT MANAGER LINKS SO CHAPTER ADMINS DON'T GET CONFUSED */
 $(document).ready(function(){
+	// EDIT EVENT MANAGER LINKS SO CHAPTER ADMINS DON'T GET CONFUSED
 	$('a[href*="/communities/community-home/manage-events"]').attr('href','https://members.simnet.org/members/my_groups.asp');
 	$('a[href*="community-payment-provider"]').remove();
-	console.log("document.ready has happened.");
+	// FIX FOOTER INSERTED IN WRONG PLACE BY MOVING IT BACK TO THE FOOTER
+	$('.section1 .footer-content').appendTo('#FOOTER');
 });
