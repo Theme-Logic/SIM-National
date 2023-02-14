4dfdfe0506fceac0ac91a7d0c972a061a597f11d
Justin Prevatte <justin@theme-logic.com>
2023-02-14 10:09:11 -0500

    now targeting BODY

--- alert.js
+++ alert.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	$('#MPOuterMost').prepend('<div class="alert alert-warning" role="alert"><h3 style="font-weight: bold;">Tech Platform Downtime Notice</h3><p>On February 16, 2023 beginning at 8 am ET, our site will be down for maintenance until approximately 12:30 PM ET. Member access will be limited during this time.</p><p>Please contact <a href="mailto:sim@simnet.org">sim@simnet.org</a> or call 800.387.9746 if you have any questions or need assistance.</p></div>');
+	$('body').prepend('<div class="alert alert-warning" role="alert"><h3 style="font-weight: bold;">Tech Platform Downtime Notice</h3><p>On February 16, 2023 beginning at 8 am ET, our site will be down for maintenance until approximately 12:30 PM ET. Member access will be limited during this time.</p><p>Please contact <a href="mailto:sim@simnet.org">sim@simnet.org</a> or call 800.387.9746 if you have any questions or need assistance.</p></div>');
 });
