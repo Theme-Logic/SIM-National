96f66c4b33afd18e50421ede69457615b644cdeb
Justin Prevatte <justin@theme-logic.com>
2023-02-17 15:17:29 -0500

    Commenting everything out

--- alert.js
+++ alert.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('.home #MPOuterHeader').append('<div class="alert alert-warning" role="alert"><h3 style="font-weight: bold;">Notice</h3><p>Our site is currently undergoing maintenance. Some features may be inaccessible and member access may be limited at times.</p><p>Please contact <a href="mailto:sim@simnet.org">sim@simnet.org</a> or call 800.387.9746 if you have any questions or need assistance.</p></div>');
-});
+});*/
